ade4fa84fd28819ea3eb9ebad68fb5d20acf2d88
alejandroevl78-blip <alejandroevl78@gmail.com>
2026-03-30 21:01:42 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('millones-app').then(function(cache) {
+    caches.open('millones-app-v2').then(function(cache) {
       return cache.addAll([
         'index.html',
         'manifest.json'
